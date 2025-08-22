862a7df664566f1a22324c8d4b3d55b272bae10d
Khushi Dinesh Gupta <Khushi Dinesh Gupta@FFE-Khushi-Dinesh-Gupta>
2025-08-22 15:28:17 +0530

    Git Extra File with changes Committed

--- src/test/java/practice/GitExtraFile.java
+++ src/test/java/practice/GitExtraFile.java
@@ -7,6 +7,7 @@ public class GitExtraFile {
 	@Test
 	public void sample() {
 		System.out.println("Hello sucessfully commited to GIT");
+		System.out.println("Chnages to be includedd in import");
 	}
 
 }
